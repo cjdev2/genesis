4fec4e050b1787617a0373ed26e43b160f82e0d8
jxv <joevargas92@gmail.com>
2017-05-26 17:38:29 -0700

    sample with a basic HTTP post & get and DB insert & get

---
+++ genesis-sample/db/migrations/0001__create_message.sql
@@ -0,0 +1,4 @@
+create table if not exists "message"
+  ( "id" serial primary key
+  , "content" text not null
+  );
---
+++ genesis-sample/executable/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified Genesis.Sample
+
+main :: IO ()
+main = Genesis.Sample.main
